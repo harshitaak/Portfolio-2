62317412ae9bb2cd2131cea47ffd2891ffc72d28
harshitaak <harshit.ea@gmail.com>
2026-04-05 03:30:51 +0530

    Quick fix. Gnyt

--- assets/js/main.js
+++ assets/js/main.js
@@ -691,8 +691,8 @@ function initBasicCustomCursor() {
   
   gsap.set(".cursor", {xPercent:-50, yPercent: -50});
 
-  let xTo = gsap.quickTo(".cursor", "x", {duration: 0.6, ease: "power3"});
-  let yTo = gsap.quickTo(".cursor", "y", {duration: 0.6, ease: "power3"});
+  let xTo = gsap.quickTo(".cursor", "x", {duration: 0.3, ease: "power3"});
+  let yTo = gsap.quickTo(".cursor", "y", {duration: 0.3, ease: "power3"});
 
   window.addEventListener("mousemove", e => {
     xTo(e.clientX);
